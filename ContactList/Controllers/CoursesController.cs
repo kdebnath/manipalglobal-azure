992d289e6a56cb44f81f2e1e88f46501c8a1fa78
Hardik Mistry <mistryhardik.05@gmail.com>
2017-03-11 09:22:46 +0530

    Init Manipal code samples

---
+++ ContactList/Controllers/CoursesController.cs
@@ -0,0 +1,42 @@
+﻿using ContactList.Models;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ContactList.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        [HttpGet]
+        [SwaggerResponse(HttpStatusCode.OK,
+            Type = typeof(IEnumerable<Course>))]
+        [Route("~/courses")]
+        public async Task<IEnumerable<Course>> Get()
+        {
+            return new List<Course>
+            {
+                new Course
+                {
+                    Id = 1,
+                    Name = "Microsoft Azure",
+                    Description = "Azure basics",
+                    Author = "Jhon Doe",
+                    Url = "http://azure.microsoft.com"
+                },
+                new Course
+                {
+                    Id = 2,
+                    Name = "Xamarin",
+                    Description = "Xamarin Fundamentals",
+                    Author = "Hardik Mistry",
+                    Url = "http://developer.xamarin.com"
+                }
+            };
+        }
+    }
+}
